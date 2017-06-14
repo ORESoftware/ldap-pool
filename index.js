8b8eb38984c2c9636260e6db828c2511e84dd9d8
Olegzandr VD <alex@oresoftware.com>
2017-06-14 12:10:54 -0700

    pdev:set

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-
-
 const ldap = require('ldapjs');
 
 let poolId = 0;
 
-function Pool(opts){
+function Pool(opts) {
 
   this.id = ++poolId;
   this.size = opts.size;
@@ -14,7 +12,10 @@ function Pool(opts){
   this.dn = opts.dn;
   this.pwd = opts.pwd;
 
-  for(let i = 0; i < this.size; i++){
+  // these are resolve functions waiting to be called
+  this.waitingForClient = [];
+
+  for (let i = 0; i < this.size; i++) {
 
     let client = ldap.createClient(this.connOpts);
 
@@ -27,13 +28,82 @@ function Pool(opts){
     client.bind(this.dn, this.pwd);
     this.inactive.push(client);
 
+
+    client.returnToPool = () => {
+
+      this.active = this.active.filter(function (v) {
+        return v !== client;
+      });
+
+      this.inactive.unshift(client);
+
+      client.__inactiveTimeoutX = setTimeout(() => {
+
+        let isDestroyable = false;
+        this.inactive = this.inactive.filter(function (v) {
+          if (v === client) {
+            isDestroyable = true;
+            return false;
+          }
+          return true;
+        });
+
+        if(isDestroyable){
+          client.unbind(function(){
+            client.destroy();
+          });
+        }
+
+      }, 3000);
+    };
+
   }
 
 }
 
+Pool.prototype.getClient = function () {
 
+  let c = this.inactive.pop();
 
-Pool.prototype.getClient = function(){
-
+  if(c){
+    this.active.unshift(c);
+    return Promise.resolve(c);
+  }
+  else{
+    return new Promise(resolve => {
+       this.unshift(this.waitingForClient(resolve));
+    });
+  }
 
 };
+
+Pool.prototype.getClientSync = function () {
+
+  let c;
+  if(c = this.inactive.pop()){
+    clearTimeout(c.__inactiveTimeoutX);
+    this.active.unshift(c);
+    return c;
+  }
+
+  let oldestActive = this.active.length - 1;
+  return this.active[oldestActive];
+
+};
+
+
+Pool.prototype.returnClientToPool = function(c){
+
+  let fn;
+
+  if(fn = this.waitingForClient.pop()){
+    fn(c);
+  }
+  else{
+    this.inactive.unshift(c);
+  }
+
+};
+
+
+module.exports = Pool;
